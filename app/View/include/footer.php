314de7701c20c8bdd31d77a5875dcdc8ae2b0e40
Lucas Akio <lucasakio.td@gmail.com>
2021-08-05 22:35:43 -0300

    view include footer

---
+++ app/View/include/footer.php
@@ -0,0 +1,19 @@
+<?php
+if (!defined("MERCEARIA2021")) // verificar se a constante criada no index, foi definida!
+{
+    header("Location: http://localhost/mercearia/paginaInvalida/index");
+    die("Erro: Página não encontrada!");
+}
+?>
+
+<footer class="footer">
+    <div class="container">
+        <p class="m-0 text-center">Copyright &copy; Lucas Akio 2021</p>
+    </div>
+</footer>
+</div>
+</div>
+</div>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="<?php echo URL; ?>app/Assets/js/cdn.js"></script>
+<script src="<?php echo URL; ?>app/Assets/fontawesome/js/all.js"></script>
