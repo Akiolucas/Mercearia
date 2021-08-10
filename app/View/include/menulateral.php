8212d199686bdcd08c7493503db46bbc06c492d2
Lucas Akio <lucasakio.td@gmail.com>
2021-08-10 00:51:36 -0300

    menu lateral de acesso a páginas do usuário

--- app/View/include/menulateral.php
+++ app/View/include/menulateral.php
@@ -8,31 +8,35 @@ if (!defined("MERCEARIA2021")) // verificar se a constante criada no index, foi 
 <nav id="menuLateral" class="col-md-3 col-lg-2 d-md-block sidebar collapse">
   <div class="sidebar-sticky">
     <ul class="nav flex-column">
-      <li class="nav-item">
-        <a class="nav-link active" href="#">
-          Página 1
+<?php
+if (isset($_SESSION['usuario_paginas']))
+{
+?>
+      <li class="nav-item active">
+        <a class="nav-link text-center" href="<?php echo URL;?>perfil">
+          <i class="fas fa-user-circle fa-7x"></i>
+          <p class="my-1"><?php echo $_SESSION['usuario_nome'];?></p>
         </a>
       </li>
+      <li class="nav-item">
+        <a class="nav-link bg-danger text-center" href="<?php echo URL; ?>sair">
+          Sair <i class="fas fa-sign-out-alt"></i>
+        </a>
+      </li>
+<?php
+  foreach ($_SESSION['usuario_paginas'] as $pagina) 
+  {
+    
+?>
       <li class="nav-item">
         <a class="nav-link" href="#">
-          Página 2
+          <?php echo $pagina; ?>
         </a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">
-          Página 3
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">
-          Página 4
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">
-          Página 5
-        </a>
-      </li>
+<?php
+  } 
+} 
+?>
     </ul>
   </div>
 </nav>
