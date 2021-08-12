ee1e7d2ba9c84aaefc1e601d62b42db2e394da68
Lucas Akio <lucasakio.td@gmail.com>
2021-08-12 00:16:01 -0300

    atualização do BD, criação da procedure produto

--- mercearia.sql
+++ mercearia.sql
@@ -78,10 +78,10 @@ DROP TABLE IF EXISTS `codigo`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `codigo` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `barra` varchar(43) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `barra` varchar(43) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '1234567890',
   PRIMARY KEY (`id`),
   UNIQUE KEY `barra_UNIQUE` (`barra`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -90,7 +90,7 @@ CREATE TABLE `codigo` (
 
 LOCK TABLES `codigo` WRITE;
 /*!40000 ALTER TABLE `codigo` DISABLE KEYS */;
-INSERT INTO `codigo` VALUES (2,'121248657410'),(1,'123458475120'),(3,'457578785410');
+INSERT INTO `codigo` VALUES (1,'1234567890'),(2,'1234567891'),(3,'1234567892'),(4,'1234567893'),(19,'1234567894'),(20,'1234567895'),(21,'1234567896'),(22,'1234567897'),(23,'1234567898'),(24,'1234567899'),(25,'1234567900'),(26,'1234567901'),(27,'1234567902'),(28,'1234567903'),(29,'1234567904'),(30,'1234567905');
 /*!40000 ALTER TABLE `codigo` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -107,8 +107,8 @@ CREATE TABLE `estoque` (
   `quantidade` int NOT NULL,
   PRIMARY KEY (`id`,`produto_id`),
   KEY `fk_estoque_produto_idx` (`produto_id`) /*!80000 INVISIBLE */,
-  CONSTRAINT `fk_estoque_produto` FOREIGN KEY (`produto_id`) REFERENCES `produto` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+  CONSTRAINT `fk_estoque_produto` FOREIGN KEY (`produto_id`) REFERENCES `produto` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -117,7 +117,7 @@ CREATE TABLE `estoque` (
 
 LOCK TABLES `estoque` WRITE;
 /*!40000 ALTER TABLE `estoque` DISABLE KEYS */;
-INSERT INTO `estoque` VALUES (1,1,30),(2,2,30),(3,3,20);
+INSERT INTO `estoque` VALUES (1,1,30),(2,2,30),(3,3,20),(4,4,30),(8,16,1),(9,17,60),(10,18,30),(11,19,30),(12,20,10),(13,21,5),(14,22,45),(15,23,100),(16,24,100),(17,25,350),(18,26,90),(19,27,250);
 /*!40000 ALTER TABLE `estoque` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -135,7 +135,7 @@ CREATE TABLE `fornecedor` (
   `dt_registro` datetime NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `cnpj_UNIQUE` (`cnpj`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -144,7 +144,7 @@ CREATE TABLE `fornecedor` (
 
 LOCK TABLES `fornecedor` WRITE;
 /*!40000 ALTER TABLE `fornecedor` DISABLE KEYS */;
-INSERT INTO `fornecedor` VALUES (1,'Marques Monte','12.123.456/0101-02','2021-07-29 08:10:30'),(2,'Panificadora Tomé','10.241.584/2014-54','2021-07-29 08:10:30');
+INSERT INTO `fornecedor` VALUES (1,'Marques Monte','12.123.456/0101-02','2021-07-29 08:10:30'),(2,'Panificadora Tomé','10.241.584/2014-54','2021-07-29 08:10:30'),(3,'Heinz','50.955.707/0001-20','2021-08-11 23:45:27');
 /*!40000 ALTER TABLE `fornecedor` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -333,9 +333,9 @@ CREATE TABLE `produto` (
   PRIMARY KEY (`id`,`fornecedor_id`,`codigo_id`),
   KEY `fk_produto_fornecedor_idx` (`fornecedor_id`),
   KEY `fk_produto_codigo_idx` (`codigo_id`),
-  CONSTRAINT `fk_produto_codigo` FOREIGN KEY (`codigo_id`) REFERENCES `codigo` (`id`),
+  CONSTRAINT `fk_produto_codigo` FOREIGN KEY (`codigo_id`) REFERENCES `codigo` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   CONSTRAINT `fk_produto_fornecedor` FOREIGN KEY (`fornecedor_id`) REFERENCES `fornecedor` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -344,9 +344,96 @@ CREATE TABLE `produto` (
 
 LOCK TABLES `produto` WRITE;
 /*!40000 ALTER TABLE `produto` DISABLE KEYS */;
-INSERT INTO `produto` VALUES (1,'Pão integral',7,2,1,0.5,'2021-07-29 08:27:37'),(2,'Pão de forma',5.5,2,2,0.5,'2021-07-29 08:27:37'),(3,'Queijo prato',46,1,3,1,'2021-07-29 08:27:37');
+INSERT INTO `produto` VALUES (1,'Pão integral',7,2,1,0.5,'2021-07-29 08:27:37'),(2,'Pão de forma',5.5,2,2,0.5,'2021-07-29 08:27:37'),(3,'Queijo prato',46,1,3,1,'2021-07-29 08:27:37'),(4,'Bolo de limão',25,2,4,1,'2021-08-11 21:24:17'),(16,'Requeijão',5,2,19,1,'2021-08-12 00:03:57'),(17,'Goiabada',4.5,2,20,0.5,'2021-08-12 00:04:50'),(18,'Bolo de maracujá',25,2,21,1,'2021-08-12 00:05:04'),(19,'Bolo de chocolate',25,2,22,1,'2021-08-12 00:05:04'),(20,'Bolo de cenoura',20,2,23,1,'2021-08-12 00:05:04'),(21,'Bolo de abacaxi',20,2,24,1,'2021-08-12 00:05:04'),(22,'Presunto Marques Monte',39.5,1,25,1,'2021-08-12 00:05:04'),(23,'Molho de tomate Heinz',4,3,26,0.4,'2021-08-12 00:05:04'),(24,'Catchup Heinz',5.5,3,27,0.26,'2021-08-12 00:05:05'),(25,'Maionese Sachê Heinz',0.5,3,28,0.8,'2021-08-12 00:05:05'),(26,'Maionese Tradicional Heinz',6.7,3,29,0.215,'2021-08-12 00:05:05'),(27,'Molho Barbecue Heinz',7,3,30,0.397,'2021-08-12 00:05:05');
 /*!40000 ALTER TABLE `produto` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `teste`
+--
+
+DROP TABLE IF EXISTS `teste`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `teste` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` double NOT NULL DEFAULT '12345',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nome_UNIQUE` (`nome`)
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `teste`
+--
+
+LOCK TABLES `teste` WRITE;
+/*!40000 ALTER TABLE `teste` DISABLE KEYS */;
+INSERT INTO `teste` VALUES (24,12345),(25,12346),(26,12347),(27,12348),(28,12349),(29,12350);
+/*!40000 ALTER TABLE `teste` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'mercearia'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `cadastrar_codigo_produto_estoque` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `cadastrar_codigo_produto_estoque`(
+nome varchar(30), 
+preco double ,
+fornecedor_id int,
+kilograma double,
+quantidade int
+)
+BEGIN
+DECLARE erro_sql TINYINT DEFAULT FALSE;
+DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET erro_sql = TRUE;
+
+IF(nome <> "" and preco <> "" and fornecedor_id <> "" and kilograma <> "" and quantidade <> "") THEN
+
+	START TRANSACTION;
+	SET @codigo = (SELECT barra + 1 AS valor FROM codigo ORDER BY id DESC LIMIT 1);
+	INSERT INTO codigo values(NULL,ifnull(@codigo,default(barra)));
+	IF erro_sql = true
+		THEN
+		SELECT 'Erro ao inserir na tabela de código' AS Mensagem;
+	ROLLBACK;
+	ELSE
+		INSERT INTO produto values(NULL, nome, preco, fornecedor_id, last_insert_id(), kilograma, now());
+		IF erro_sql = true
+			THEN
+			SELECT 'Erro ao inserir na tabela de produto' AS Mensagem;
+		ROLLBACK;
+		ELSE
+			INSERT INTO estoque values(null,last_insert_id(),quantidade);
+			IF erro_sql = true
+				THEN
+				SELECT 'Erro ao inserir na tabela de estoque' AS Mensagem;
+			ROLLBACK;
+			ELSE
+			 SELECT 'Cadastro efetuado com sucesso' AS Mensagem;
+			  COMMIT;
+			END IF;
+		END IF;
+	END IF;
+ELSE
+	SELECT 'Parâmetros vazios' AS Mensagem;
+END IF;
+                
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -357,4 +444,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-08-10  0:53:26
+-- Dump completed on 2021-08-12  0:11:29
