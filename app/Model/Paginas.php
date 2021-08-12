706b77fa33d53d9523847ee2efd6e452b732d81a
Lucas Akio <lucasakio.td@gmail.com>
2021-08-12 19:45:05 -0300

    Configurações do munu lateral

--- app/Model/Paginas.php
+++ app/Model/Paginas.php
@@ -10,6 +10,7 @@ namespace App\Model;
 class Paginas extends Model
 {
     private $paginas;
+    private $removerPgMenu = array("Perfil", "Login", "PaginaInvalida");
 
     final public function acessoPaginas(array $id)
     {
@@ -20,10 +21,19 @@ class Paginas extends Model
         ON f.pg_privada_id = p.id 
         WHERE f.funcionario_id = :id",$id,false);
         
-        /*Formata o array bidimensional para um vetor*/
+        /*remove do menu lateral páginas expecificas, e tranformar o array bidimensional em um vetor*/
         foreach ($this->paginas as $key => $value){
-            array_push($this->paginas,$value['pagina']);
-            unset($this->paginas[$key]);
+            
+            foreach($this->removerPgMenu as $item)
+            {
+                if(in_array($item,$value)){
+                    unset($this->paginas[$key]);
+                }
+                else{
+                    array_push($this->paginas,$value['pagina']);
+                    unset($this->paginas[$key]);
+                }
+            }  
         }
         sort($this->paginas);
         // fim 
