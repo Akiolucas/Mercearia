0f4dfa36a446c890d218f34e841c0e5b13978ad2
Lucas Akio <lucasakio.td@gmail.com>
2021-08-12 22:45:04 -0300

    correções no loop, função para bloqueio de paginas

--- app/Model/Paginas.php
+++ app/Model/Paginas.php
@@ -10,6 +10,8 @@ namespace App\Model;
 class Paginas extends Model
 {
     private $paginas;
+    private array $pgPrivadas = array();
+    private array $pgPublicas = array();
     private $removerPgMenu = array("Perfil", "Login", "PaginaInvalida");
 
     final public function acessoPaginas(array $id)
@@ -21,24 +23,40 @@ class Paginas extends Model
         ON f.pg_privada_id = p.id 
         WHERE f.funcionario_id = :id",$id,false);
         
-        /*remove do menu lateral páginas expecificas, e tranformar o array bidimensional em um vetor*/
-        foreach ($this->paginas as $key => $value){
-            
-            foreach($this->removerPgMenu as $item)
-            {
-                if(in_array($item,$value)){
-                    unset($this->paginas[$key]);
-                }
-                else{
-                    array_push($this->paginas,$value['pagina']);
-                    unset($this->paginas[$key]);
-                }
-            }  
+        
+        /*tranformar o array bidimensional em um vetor e remove do menu lateral páginas expecificas*/
+        foreach($this->paginas as $key => $value)
+        {
+            array_push($this->paginas,$value['pagina']);
+            unset($this->paginas[$key]);       
+        }
+        foreach($this->removerPgMenu as $item)
+        {
+            if(in_array($item, $this->paginas))
+            {   
+                var_dump($item);
+                unset($this->paginas[array_search($item,$this->paginas)]);
+            }
         }
         sort($this->paginas);
         // fim 
-
         return $this->paginas;
+    }
+    private function listarPaginas()
+    {
+        $this->pgPrivadas = parent::projetarTodos("SELECT nome FROM pg_privada");
+        $this->pgPublicas = parent::projetarTodos("SELECT nome FROM pg_publica");
+        
+    }
+
+    public function permissaoPaginas(): array
+    {
+        $this->listarPaginas();
+        $paginas = array(
+            'publicas' => $this->pgPublicas,
+            'privadas' => $this->pgPrivadas
+        );
+        return $paginas;
     }
 
 }
