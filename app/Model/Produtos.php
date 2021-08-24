5159d5e3fcf87c6b01625f65828cd40dba55ac2c
Lucas Akio <lucasakio.td@gmail.com>
2021-08-23 23:11:12 -0300

    metodo editar, atualizar e deletar em desenvolvimento

--- app/Model/Produtos.php
+++ app/Model/Produtos.php
@@ -9,6 +9,8 @@
 
     class Produtos extends Model
     {
+        private $form_obrigatorio = array('id','nome','preco','fornecedor_id','codigo_id','kilograma','dt_registro','btn_atualizar');
+        private array $form_valido;
         public function listar(): array
         {
             $listar = parent::projetarTodos(
@@ -20,6 +22,44 @@
             ON p.codigo_id = c.id");
 
             return $listar;
+        }
+
+        public function editar($id): array
+        {
+            $produto = parent::projetarExpecifico(
+            "SELECT p.id, p.nome, p.preco AS preço, f.nome AS fornecedor, c.barra AS código, p.kilograma, p.dt_registro
+            FROM produto p
+            INNER JOIN fornecedor f
+            ON p.fornecedor_id = f.id
+            INNER JOIN codigo c
+            ON p.codigo_id = c.id
+            WHERE p.id = :id",$id,false);
+
+            if(!empty($produto)){
+                return $produto;
+            }
+            $msg = "Produto não encontrado";
+            $_SESSION['msg'] = parent::alertaFalha($msg);
+            header("location:". URL . "produtos/index");
+            exit();
+        }
+        public function atualizar($dados)
+        {
+            if(parent::existeCamposFormulario($dados,$this->form_obrigatorio)){
+                array_push($this->form_valido,
+                parent::valida_int(array($dados['id'],$dados['codigo_id'])),
+                parent::valida_date(array($dados['dt_registro'])),
+                parent::valida_float(array($dados['preco'],$dados['kilograma']))
+                );
+                var_dump($this->form_valido);
+                exit();
+            }
+
+        }
+        
+        public function excluir()
+        {
+
         }
     }
 
