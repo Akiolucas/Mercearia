d8de8e645370a61523165fc827cb481a7ca72404
Lucas Akio <lucasakio.td@gmail.com>
2021-08-25 23:47:01 -0300

    implementado o método de atualizar e validações

--- app/Model/Produtos.php
+++ app/Model/Produtos.php
@@ -10,7 +10,8 @@
     class Produtos extends Model
     {
         private $form_obrigatorio = array('id','nome','preco','fornecedor_id','codigo_id','kilograma','dt_registro','btn_atualizar');
-        private array $form_valido;
+        private int $form_obrigatorio_quantidade;
+        private array $form_valido = array();
         public function listar(): array
         {
             $listar = parent::projetarTodos(
@@ -27,8 +28,8 @@
         public function editar($id): array
         {
             $produto = parent::projetarExpecifico(
-            "SELECT p.id, p.nome, p.preco AS preço, f.nome AS fornecedor, c.barra AS código, p.kilograma, p.dt_registro
-            FROM produto p
+            "SELECT p.id, p.nome, p.preco, p.fornecedor_id, f.nome AS fornecedor, p.codigo_id, c.barra AS codigo, p.kilograma, p.dt_registro
+            FROM produto p 
             INNER JOIN fornecedor f
             ON p.fornecedor_id = f.id
             INNER JOIN codigo c
@@ -43,16 +44,44 @@
             header("location:". URL . "produtos/index");
             exit();
         }
+        
         public function atualizar($dados)
-        {
-            if(parent::existeCamposFormulario($dados,$this->form_obrigatorio)){
+        {   
+            $this->form_obrigatorio_quantidade = count($this->form_obrigatorio);
+
+            if(parent::existeCamposFormulario($dados,$this->form_obrigatorio,$this->form_obrigatorio_quantidade)){
                 array_push($this->form_valido,
                 parent::valida_int(array($dados['id'],$dados['codigo_id'])),
                 parent::valida_date(array($dados['dt_registro'])),
                 parent::valida_float(array($dados['preco'],$dados['kilograma']))
                 );
-                var_dump($this->form_valido);
-                exit();
+
+                if(parent::formularioValido($this->form_valido))
+                {
+                    $dados['dt_registro'] = date('Y-m-d H:i:s');
+                    unset($dados['btn_atualizar']);
+
+                    parent::implementar(
+                    "UPDATE produto 
+                    SET
+                    nome =:nome,
+                    preco = :preco,
+                    fornecedor_id = :fornecedor_id,
+                    codigo_id = :codigo_id,
+                    kilograma = :kilograma,
+                    dt_registro = :dt_registro
+                    WHERE id = :id",
+                    $dados);
+
+                    $msg = "Produto atualizado com sucesso";
+                    $_SESSION['msg'] = parent::alertaSucesso($msg);
+                }
+                else{
+                    $msg = "Não foi possível atualizar o produto tente novamente";
+                    $_SESSION['msg'] = parent::alertaFalha($msg);
+                    header("location:". URL . "produtos/editar/&id=". $dados['id']);
+                    exit();
+                }
             }
 
         }
