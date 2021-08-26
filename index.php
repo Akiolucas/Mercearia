dcb3fd093aa34d82343a515d0e7cd737c278af61
Lucas Akio <lucasakio.td@gmail.com>
2021-08-25 23:38:41 -0300

    adicionado fuso hórario padrão para data e hora

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
-define("MERCEARIA2021", true); //altere o nome dessa constante para um nome mais difícil. 
+define("MERCEARIA2021", true); //altere o nome dessa constante para um nome mais difícil.
+date_default_timezone_set('America/Sao_Paulo'); 
 
 require './vendor/autoload.php';
 $url = new Core\ConfigController();
