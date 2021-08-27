2569ddc1c932b8360fdc766e373848c870202354
Lucas Akio <lucasakio.td@gmail.com>
2021-08-26 23:45:07 -0300

    métodos de listagem de fornecedores via ajax

--- app/Assets/js/produtos-ajax.js
+++ app/Assets/js/produtos-ajax.js
@@ -1,26 +1,57 @@
-$(document).ready(function(){
-
+function editarProdutos()
+{
     let fornecedor_id = $("#option_padrao").val();
 
-    $.post("http://localhost/mercearia/produtos/fornecedorAjax",function(result, status){
-       let objeto = JSON.parse(result);
+    $.post("http://localhost/mercearia/produtos/fornecedorAjax",function(result){
+        let objeto = JSON.parse(result);
         let text = "";
 
         objeto.forEach(listar);
 
         $selected = $('#form_option').val();
 
-       function listar(item, index)
-       {
+        function listar(item)
+        {
         if(item['id'] != fornecedor_id){
             text +="<option value=" + item['id'] + ">" + item['nome'] + "</option>"; 
         }
-       }
-       
-       $("#form_fornecedor").append(text);
-
+        }
+        
+        $("#form_fornecedor").append(text);
     })
 
-});
+    .fail(function(){
+        if(confirm('Ops, não foi possível carregar todos os dados, por favor Recarregue a página novamente!'))
+        {
+            document.location.reload();
+        }        
+    });
+}
+    
+function indexProdutos()
+{
+    $.post("http://localhost/mercearia/produtos/fornecedorAjax",function(result){
+        let objeto = JSON.parse(result);
+        let text = "";
+
+        objeto.forEach(listar);
+
+        function listar(item)
+        {
+            text +="<option value=" + item['id'] + ">" + item['nome'] + "</option>"; 
+        }
+        
+        $("#form-fornecedor").append(text);
+    })
+    
+    .fail(function(){
+        if(confirm('Ops, não foi possível carregar todos os dados, por favor Recarregue a página novamente!'))
+        {
+            document.location.reload();
+        }        
+    });
+    
+
+} 
 
 
