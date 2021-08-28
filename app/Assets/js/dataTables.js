98a31f80ba14be3e50ce6249da304fa166d134ad
Lucas Akio <lucasakio.td@gmail.com>
2021-08-28 13:34:19 -0300

    ordenar produtos pela data de cadastro

--- app/Assets/js/dataTables.js
+++ app/Assets/js/dataTables.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $('#listar-produtos').DataTable( {
         "language": {
             "url": "https://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Portuguese-Brasil.json"
-        }
+        },
+        "order":[[7,'desc']]
     });
 });
