f90fe0d436f82617f67d5f16beb3df1be095c28d
Lucas Akio <lucasakio.td@gmail.com>
2021-08-29 12:25:24 -0300

    modal de confirmação para excluir produto

--- app/Assets/js/eventos.js
+++ app/Assets/js/eventos.js
@@ -7,3 +7,12 @@ $('#btn-menu').click( function () {
   $('#menuLateral').on('hidden.bs.collapse', function () {
     $('#btn-menu').removeClass("border-btn-menu");
   })
+
+  $('td #link_excluir_p').click(function(e){
+    e.preventDefault();
+    var href = $(this).attr('href');
+    $('#confirm-delete').modal({show:true});
+    $('#excluir_ok').attr('href',href);
+  });
+
+
