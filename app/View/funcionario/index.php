f1166e5cb4947305167f913f64e370852c34ba60
Lucas Akio <lucasakio.td@gmail.com>
2021-09-04 16:23:13 -0300

    View: lista de funcionários

---
+++ app/View/funcionario/index.php
@@ -0,0 +1,196 @@
+<?php
+if (!defined("MERCEARIA2021")) // verificar se a constante criada no index, foi definida!
+{
+    header("Location: http://localhost/mercearia/paginaInvalida/index");
+    die("Erro: Página não encontrada!");
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <script src="<?php echo URL; ?>app/Assets/js/cssBootstrap.js"></script>
+    <link href="<?php echo URL; ?>app/Assets/css/mercearia.css" rel="stylesheet">
+    <link href="<?php echo URL; ?>app/Assets/fontawesome/css/all.css" rel="stylesheet">
+    <link href="<?php echo URL; ?>app/Assets/image/logo/favicon.ico" rel="shortcut icon">
+    <link href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css" rel="stylesheet">
+    <title>Funcionários | Mercearia</title>
+</head>
+<?php
+
+include_once "app/View/include/header.php";
+
+?>
+<div class="container-fluid">
+    <div class="row">
+        <?php
+        include_once "app/View/include/menulateral.php";
+        ?>
+        <div class="col-md-9 ml-auto col-lg-10 principal">
+            <main role="main" class="m-2">
+                <?php
+                if (isset($_SESSION['msg'])) {
+                    echo $_SESSION['msg'];
+                    unset($_SESSION['msg']);
+                }
+
+                ?>
+                <h2 class="text-center mb-3">Tabela de Funcionários</h2>
+                <button type="button" class="btn btn-success m-button-table" data-toggle="modal" data-target="#modalCadastrar"><i class="fas fa-pen"></i> Cadastrar</button>
+                <div class="table-responsive">
+                    <table id="listar-funcionarios" class="table table-hover text-center tabela-personalizada">
+                        <thead class="bg-info">
+                            <tr>
+                                <th>Id</th>
+                                <th>Nome</th>
+                                <th>Cargo</th>
+                                <th>Acesso</th>
+                                <th>Ativo</th>
+                                <th>Registro</th>
+                                <th>Editar</th>
+                                <th>Excluir</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            foreach ($this->dados as $coluna => $item) {
+                            ?> <tr>
+                                    <td><?php echo $item['id']; ?></td>
+                                    <td><?php echo $item['nome']; ?></td>
+                                    <td><?php echo $item['cargo']; ?></td>
+                                    <td><?php echo $item['acesso']; ?></td>
+                                    <td><?php echo $item['ativo'] == 1 ? "Sim" : "Não"; ?></td>
+                                    <td data-order="<?php echo date('Y/m/d H:i:s', strtotime($item['dt_registro'])); ?>"><?php echo date('d/m/Y', strtotime($item['dt_registro'])); ?></td>
+                                    <td>
+                                        <a href="<?php echo URL; ?>funcionario/editar/&id=<?php echo $item['id'] ?>" class="btn btn-warning col-mb-3"><i class="fas fa-edit"></i></a>
+                                    </td>
+                                    <td>
+                                        <a href="<?php echo URL; ?>funcionario/excluir/&id=<?php echo $item['id'] ?>" class="btn btn-danger col-mb-3" id='link_excluir'><i class="fas fa-trash-alt"></i></a>
+                                    </td>
+                                </tr>
+                            <?php
+                            }
+                            ?>
+                        </tbody>
+                        <tfoot class="bg-info">
+                            <tr>
+                                <th>Id</th>
+                                <th>Nome</th>
+                                <th>Cargo</th>
+                                <th>Acesso</th>
+                                <th>Ativo</th>
+                                <th>Registro</th>
+                                <th>Editar</th>
+                                <th>Excluir</th>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
+                <div class="modal fade" id="modalCadastrar" tabindex="-1" role="dialog" aria-labelledby="cadastrarFuncionario" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title text-center w-100" id="cadastrarFucionario">Funcionário</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <form action="<?php echo URL; ?>funcionario/cadastrar" method="POST">
+                                <div class="modal-body">
+                                    <div class="form-row">
+                                        <div class="form-group col-9">
+                                            <label for="form-nome" class="col-form-label">Nome:</label>
+                                            <input type="text" name="nome" id="form-nome" class="form-control" required>
+                                        </div>
+                                        <div class="form-group col-3">
+                                            <label for="form-ativo" class="col-form-label">Ativo:</label>
+                                            <select name="ativo" id="form-ativo" class="form-control" required>
+                                                <option value="1" selected>Sim</option>
+                                                <option value="0">Não</option>
+                                            </select>
+                                        </div>
+
+                                    </div>
+                                    <div class="form-row">
+                                        <div class="form-group col-8">
+                                            <label for="form-cargo" class="col-form-label">Cargo:</label>
+                                            <select name="cargo_id" class="form-control" id="form-cargo" required>
+                                            </select>
+                                        </div>
+                                        <div class="form-group col-4">
+                                            <label for="form-nivel" class="col-form-label">Nível:</label>
+                                            <select name="nivel_id" id="form-nivel" class="form-control" required>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="form-row">
+                                        <div class="form-group col-12">
+                                            <label for="form-credencial" class="col-form-label">Credencial:</label>
+                                            <input type="text" name="credencial" id="form-credencial" class="form-control" required>
+                                        </div>
+                                    </div>
+                                    <div class="form-row">
+                                        <div class="form-group col-12">
+                                            <label for="form-senha" class="col-form-label">Senha:</label>
+                                            <input type="text" name="senha" id="form-senha" class="form-control" required>
+                                        </div>
+                                    </div>
+                                    <div class="form-row">
+                                        <div class="form-group col-12">
+                                            <label for="form-pg-privada-id" class="col-form-label">Selecione as páginas de acesso:</label>
+                                           <div id="form-pg-privada-id">
+                                           </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <div class="form-row">
+                                        <div class=" form-group col-6">
+                                            <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
+                                        </div>
+                                        <div class="form-group col-6">
+                                            <input type="submit" class="btn btn-success" name="btn_cadastrar" value="Cadastrar">
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal fade" id="confirm-delete" tabindex="-1" aria-labelledby="modalExcluirFuncionario" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="modalExcluirFuncionario">Excluir funcionário</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <p>Deseja realmente excluir esse funcionário?</p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-info" data-dismiss="modal">Cancelar</button>
+                                <a class="btn btn-danger" id="excluir_ok">Excluir</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+            <?php
+            include_once "app/View/include/footer.php";
+            ?>
+            <script src="<?php echo URL; ?>app/Assets/js/eventos.js"></script>
+            <script src="<?php echo URL; ?>app/Assets/jquery/jquery.dataTables.min.js"></script>
+            <script src="<?php echo URL; ?>app/Assets/js/dataTables.js"></script>
+            <script src="<?php echo URL; ?>app/Assets/js/funcionario-ajax.js"></script>
+            <script>
+                window.addEventListener("load", dadosCadastro());
+            </script>
+            </body>
+
+</html>
