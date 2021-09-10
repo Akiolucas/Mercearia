fa740c54ce48a0c0ae207f631da90b19a3e57cfd
Lucas Akio <lucasakio.td@gmail.com>
2021-09-09 21:05:14 -0300

    View: Editar funcionário concluída

--- app/View/funcionario/editar.php
+++ app/View/funcionario/editar.php
@@ -41,35 +41,92 @@ include_once "app/View/include/header.php";
 
                 <div id="form-atualizar">
                     <h1 class="text-center mt-2">Funcionário</h1>
-                    <form action="<?php echo URL; ?>funcionario/atualizar" method="POST">
+                    <form action="<?php echo URL; ?>funcionario/atualizar" method="POST" id='form-atualizar-funcionario'>
                         <div class="form-row">
                             <div class="form-group col-8">
                                 <input type="hidden" name="id" value="<?php echo $funcionario['id'] ?>">
                                 <label for="form_nome">Nome</label>
-                                <input type="text" name="nome" class="form-control" id="form_nome" value="<?php echo $funcionario['nome'] ?>">
+                                <input type="text" name="nome" class="form-control <?php echo isset($_SESSION['Erro_form']['nome']) ? 'is-invalid' : '' ?>" aria-describedby="serverNome" id="form-nome" value="<?php echo $funcionario['nome'] ?>">
+                                <?php
+                                if (isset($_SESSION['Erro_form']['nome'])) { ?>
+                                    <div id='serverNome' class="invalid-feedback">
+                                        <?php
+                                        echo $_SESSION['Erro_form']['nome'];
+                                        unset($_SESSION['Erro_form']['nome']);
+                                        ?>
+                                    </div>
+                                <?php
+                                } ?>
                             </div>
 
                             <div class="form-group col-4">
                                 <label for="form_ativo">Ativo</label>
-                                <select name="ativo" class="form-control" id="form_ativo">                        
-                                     <option value="1"<?php echo $funcionario['ativo'] == 1?"selected":""?>>Sim</option>
-                                     <option value="0"<?php echo $funcionario['ativo'] == 0?"selected":""?>>Não</option>
+                                <select name="ativo" class="form-control <?php echo isset($_SESSION['Erro_form']['ativo']) ? 'is-invalid' : '' ?>" aria-describedby="serverAtivo" id="form-ativo" required>
+                                    <option value="1" <?php echo $funcionario['ativo'] == 1 ? "selected" : "" ?>>Sim</option>
+                                    <option value="0" <?php echo $funcionario['ativo'] == 0 ? "selected" : "" ?>>Não</option>
                                 </select>
+                                <?php if (isset($_SESSION['Erro_form']['ativo'])) { ?>
+
+                                    <div id='serverAtivo' class="invalid-feedback">
+                                        <?php
+                                        echo $_SESSION['Erro_form']['ativo'];
+                                        unset($_SESSION['Erro_form']['ativo']);
+                                        ?>
+                                    </div>
+                                <?php
+                                } ?>
                             </div>
                         </div>
                         <div class="form-row">
                             <div class="form-group col-8">
                                 <label for="form_cargo">Cargo</label>
-                                <select name="cargo_id" class="form-control" id="form_cargo_id">
+                                <select name="cargo_id" class="form-control<?php echo isset($_SESSION['Erro_form']['cargo_id']) ? 'is-invalid' : '' ?>" id="form-cargo" aria-describedby="serverCargo" required>
                                     <option value="<?php echo $funcionario['cargo_id'] ?>" id="option_padrao_cargo" selected><?php echo $funcionario['cargo'] ?></option>
                                 </select>
+                                <?php if (isset($_SESSION['Erro_form']['cargo_id'])) { ?>
+
+                                    <div id='serverCargo' class="invalid-feedback">
+                                        <?php
+                                        echo $_SESSION['Erro_form']['cargo_id'];
+                                        unset($_SESSION['Erro_form']['cargo_id']);
+                                        ?>
+                                    </div>
+                                <?php
+                                } ?>
                             </div>
 
                             <div class="form-group col-4">
                                 <label for="form_nivel">Nível</label>
-                                <select name="nivel_id" class="form-control" id="form_nivel">
+                                <select name="nivel_id" id="form-nivel" class="form-control <?php echo isset($_SESSION['Erro_form']['nivel_id']) ? 'is-invalid' : '' ?>" aria-describedby="serverNivel" required>
                                     <option value="<?php echo $funcionario['nivel_id'] ?>" id="option_padrao_nivel" selected><?php echo $funcionario['nivel'] ?></option>
                                 </select>
+                                <?php if (isset($_SESSION['Erro_form']['nivel_id'])) { ?>
+
+                                    <div id='serverNivel' class="invalid-feedback">
+                                        <?php
+                                        echo $_SESSION['Erro_form']['nivel_id'];
+                                        unset($_SESSION['Erro_form']['nivel_id']);
+                                        ?>
+                                    </div>
+                                <?php
+                                } ?>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col-12">
+                                <label for="form-pg-privada-id" class="form-check-label">Selecione as páginas de acesso:</label>
+                                <div id="form-pg-privada-id" class="form-check <?php echo isset($_SESSION['Erro_form']['pg_privada_id']) ? 'is-invalid' : '' ?>" aria-describedby="serverPg_privada">
+                                </div>
+                                <?php if (isset($_SESSION['Erro_form']['pg_privada_id'])) { ?>
+
+                                    <div id='serverPg_privada' class="invalid-feedback">
+                                        <?php
+                                        echo $_SESSION['Erro_form']['pg_privada_id'];
+                                        unset($_SESSION['Erro_form']['pg_privada_id']);
+                                        ?>
+                                    </div>
+                                <?php
+                                } ?>
                             </div>
                         </div>
 
