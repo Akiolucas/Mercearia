efa2a01e5187b0bcad6595b5068d04ec252eb8a0
Lucas Akio <lucasakio.td@gmail.com>
2021-09-09 22:17:25 -0300

    Model: Cargo, validação atualizada.

--- app/Model/Cargo.php
+++ app/Model/Cargo.php
@@ -47,7 +47,7 @@ class Cargo extends Model
 
         if (parent::existeCamposFormulario($dados, $this->form_obrigatorio, $this->form_obrigatorio_quantidade)){
 
-            $this->form_valido = array(parent::valida_tamanho(array($dados['nome']),array(45)));
+            $this->form_valido = array(parent::valida_tamanho($dados['nome'],'nome','*Informe um cargo, limite 45 caracteres',45,0));
             if (parent::formularioValido($this->form_valido)) 
             {
                 unset($dados['btn_cadastrar']);
@@ -59,25 +59,32 @@ class Cargo extends Model
                     $_SESSION['msg'] = parent::alertaSucesso($msg);
 
                 } catch (PDOException $e) {
+                    $_SESSION['form'] = $dados;
+                    $_SESSION['script'] = "<script>$('#modalCadastrar').modal('show');</script>";
                     $msg = "Não foi possível cadastrar o cargo!";
-                    $_SESSION['msg'] = parent::alertaFalha($msg);
+                    $_SESSION['alerta'] = parent::alertaFalha($msg);
                 }
             }
             else {
+                $_SESSION['form'] = $dados;
+                $_SESSION['script'] = "<script>$('#modalCadastrar').modal('show');</script>";
                 $msg = "Não foi possível cadastrar o cargo, verifique os dados e tente novamente!";
-                $_SESSION['msg'] = parent::alertaFalha($msg);
+                $_SESSION['alerta'] = parent::alertaFalha($msg);
+                
             }
         }
         else{
+            $_SESSION['form'] = $dados;
+            $_SESSION['script'] = "<script>$('#modalCadastrar').modal('show');</script>";
             $msg = "Preencha todos os campos corretamente e tente novamente!";
-            $_SESSION['msg'] = parent::alertaFalha($msg);
+            $_SESSION['alerta'] = parent::alertaFalha($msg);
         }
     }
 
     public function excluir($id): void
     {
 
-       array_push($this->form_valido,parent::valida_int($id));
+       array_push($this->form_valido,parent::valida_int($id,'id','*Id do cargo inválido',1));
 
        if(parent::formularioValido($this->form_valido))
        {
@@ -112,8 +119,10 @@ class Cargo extends Model
 
         if(parent::existeCamposFormulario($dados,$this->form_obrigatorio,$this->form_obrigatorio_quantidade))
         {
-            array_push($this->form_valido,parent::valida_int(array($dados['id'])),parent::valida_tamanho(array($dados['nome']),array(45)));
-
+            array_push($this->form_valido,
+            parent::valida_int($dados['id'],'id','*Id do cargo inválido',1),
+            parent::valida_tamanho($dados['nome'],'nome','*Informe o nome do cargo corretamente, limite 45 caracteres',45,2));
+        
             if(parent::formularioValido($this->form_valido))
             {
                 unset($dados['btn_atualizar']);
