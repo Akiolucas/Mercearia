fb722b51767895bf0c67d1cf9d4bf9b5feff8b84
Lucas Akio <lucasakio.td@gmail.com>
2021-09-09 21:09:01 -0300

    Model: funcionário concluída

--- app/Model/Funcionario.php
+++ app/Model/Funcionario.php
@@ -62,63 +62,88 @@ class Funcionario extends Model
         $this->form_obrigatorio_quantidade = count($this->form_obrigatorio);
 
         if (parent::existeCamposFormulario($dados, $this->form_obrigatorio, $this->form_obrigatorio_quantidade)) {
-            $this->form_valido = array(
-                parent::valida_tamanho(array($dados['nome'], $dados['credencial'], $dados['senha']), array(70, 20, 12)),
-                parent::valida_bool(array($dados['ativo'])),
-                parent::valida_int(array($dados['cargo_id'], $dados['nivel_id'])),
-                parent::valida_int($dados['pg_privada_id'])
-            );
 
-            if (parent::formularioValido($this->form_valido)) {
-                unset($dados['btn_cadastrar']);
-                $dados['id'] = NULL;
-                $dados['senha'] = password_hash($dados['senha'], PASSWORD_DEFAULT);
-                $dados['dt_registro'] = date('Y-m-d H:i:s');
-                $this->funcionario_paginas = $dados['pg_privada_id'];
-                unset($dados['pg_privada_id']);
-
-                $this->conn->beginTransaction();
-                try{
-
-                    parent::implementar(
-                        "INSERT INTO funcionario 
-                        VALUES (:id, :nome, :cargo_id, :nivel_id, :credencial, :senha, :ativo, :dt_registro)", $dados);
-
-                    $this->funcionario_id = intval($this->conn->lastInsertId());
-
-                    foreach ($this->funcionario_paginas as $pagina) {
-                        $id_funcionario_pagina = array(
-                            'funcionario_id' => $this->funcionario_id,
-                            'pg_privada_id' => $pagina,
-                            'dt_registro' => date('Y-m-d H:i:s')
-                        );
-
-                        parent::implementar("INSERT INTO funcionario_pg_privada VALUES (:funcionario_id, :pg_privada_id, :dt_registro)", $id_funcionario_pagina);
-                    };
-
-                    $msg = "Funcionário cadastrado com sucesso!";
-                    $_SESSION['msg'] = parent::alertaSucesso($msg);
-                    $this->conn->commit();
-                }
-                catch(PDOException $e){
-                    $this->conn->rollBack();
-                    $msg = "Não foi possível cadastrar o funcionário!";
-                    $_SESSION['msg'] = parent::alertaFalha($msg);
-                }
-                
-            } else {
-                $msg = "Não foi possível cadastrar o funcionário, verifique os dados e tente novamente!";
-                $_SESSION['msg'] = parent::alertaFalha($msg);
+            $paginas_privadas = array();
+            foreach($dados['pg_privada_id'] as $item)
+            {   
+                array_push($paginas_privadas,parent::valida_int($item,'pg_privada_id','*Selecione pelo menos uma opção',0));
             }
+
+            if(parent::formularioValido($paginas_privadas))
+            {
+                $this->form_valido = array(
+                    parent::valida_tamanho($dados['nome'],'nome','*O limite máximo permitido é 70 caracteres',70,0),
+                    parent::valida_tamanho($dados['credencial'],'credencial','*A credencial deve conter entre 8 a 20 caracteres',20,8),
+                    parent::valida_tamanho($dados['senha'],'senha','*A senha deve conter entre 8 a 64 caracteres',64,8),
+                    parent::valida_bool($dados['ativo'],'ativo','*Selecione uma opção'),
+                    parent::valida_int($dados['cargo_id'],'cargo_id','*Selecione uma opção',0),
+                    parent::valida_int($dados['nivel_id'],'nivel_id','*Selecione uma opção',0)
+                );
+                if (parent::formularioValido($this->form_valido))
+                {
+                
+                    $this->conn->beginTransaction();
+                    try{
+                        unset($dados['btn_cadastrar']);
+                        $dados['id'] = NULL;
+                        $dados['senha'] = password_hash($dados['senha'], PASSWORD_DEFAULT);
+                        $dados['dt_registro'] = date('Y-m-d H:i:s');
+                        $this->funcionario_paginas = $dados['pg_privada_id'];
+                        unset($dados['pg_privada_id']);
+
+                        parent::implementar(
+                            "INSERT INTO funcionario 
+                            VALUES (:id, :nome, :cargo_id, :nivel_id, :credencial, :senha, :ativo, :dt_registro)", $dados);
+                           
+                        $this->funcionario_id = intval($this->conn->lastInsertId());
+    
+                        foreach ($this->funcionario_paginas as $pagina) {
+                            $id_funcionario_pagina = array(
+                                'funcionario_id' => $this->funcionario_id,
+                                'pg_privada_id' => $pagina,
+                                'dt_registro' => date('Y-m-d H:i:s')
+                            );
+                            parent::implementar("INSERT INTO funcionario_pg_privada VALUES (:funcionario_id, :pg_privada_id, :dt_registro)", $id_funcionario_pagina);
+                            
+                        };
+    
+                        $msg = "Funcionário cadastrado com sucesso!";
+                        $_SESSION['msg'] = parent::alertaSucesso($msg);
+                        $this->conn->commit();
+                    }
+                    catch(PDOException $e){
+                        $this->conn->rollBack();
+                        unset($dados['senha']);
+                        $_SESSION['form'] = $dados;
+                        $_SESSION['script'] = "<script>$('#modalCadastrar').modal('show');</script>";
+                        $msg = "Não foi possível cadastrar o funcionário!";
+                        $_SESSION['alerta'] = parent::alertaFalha($msg);
+                    }                   
+                }
+                else {
+                    $_SESSION['form'] = $dados;
+                    $_SESSION['script'] = "<script>$('#modalCadastrar').modal('show');</script>";
+                    $msg = "Não foi possível cadastrar o funcionário, verifique os dados e tente novamente!";
+                    $_SESSION['alerta'] = parent::alertaFalha($msg);
+                }
+            }
+            else{
+                $_SESSION['form'] = $dados;
+                $_SESSION['script'] = "<script>$('#modalCadastrar').modal('show');</script>";
+                $msg = "Preencha todos os campos corretamente e tente novamente!";
+                $_SESSION['alerta'] = parent::alertaFalha($msg);
+            }       
         } else {
+            $_SESSION['form'] = $dados;
+            $_SESSION['script'] = "<script>$('#modalCadastrar').modal('show');</script>";
             $msg = "Preencha todos os campos corretamente e tente novamente!";
-            $_SESSION['msg'] = parent::alertaFalha($msg);
+            $_SESSION['alerta'] = parent::alertaFalha($msg);
         }
     }
 
     public function excluir($id): void
     {
-        array_push($this->form_valido, parent::valida_int($id));
+        array_push($this->form_valido, parent::valida_int($id,'funcionario_id','funcionário não encontrado',0));
 
         if (parent::formularioValido($this->form_valido)) {
             try {
@@ -145,41 +170,86 @@ class Funcionario extends Model
 
     public function atualizar($dados): void
     {
-        $this->form_obrigatorio = array('id', 'nome','cargo_id','nivel_id','ativo','btn_atualizar');
+        $this->form_obrigatorio = array('id', 'nome','cargo_id','nivel_id','ativo','pg_privada_id','btn_atualizar');
         $this->form_obrigatorio_quantidade = count($this->form_obrigatorio);
 
-        if (parent::existeCamposFormulario($dados, $this->form_obrigatorio, $this->form_obrigatorio_quantidade)) {
-            array_push($this->form_valido, 
-            parent::valida_int(array($dados['id'],$dados['cargo_id'],$dados['nivel_id']),
-            parent::valida_tamanho(array($dados['nome']), array(70))),
-            parent::valida_bool(array($dados['ativo'])));
-            
-            if (parent::formularioValido($this->form_valido)) {
-                unset($dados['btn_atualizar']);
-                $dados['dt_registro'] = date('Y-m-d H:i:s');
-                try {
-                    parent::implementar(
-                    "UPDATE funcionario SET
-                    id =:id, nome =:nome, cargo_id = :cargo_id, nivel_id = :nivel_id, ativo = :ativo, dt_registro = :dt_registro
-                    WHERE id =:id", $dados);
+        if (parent::existeCamposFormulario($dados, $this->form_obrigatorio, $this->form_obrigatorio_quantidade)) 
+        {
+            $paginas_privadas = array();
+            foreach($dados['pg_privada_id'] as $item)
+            {   
+                array_push($paginas_privadas,parent::valida_int($item,'pg_privada_id','*Selecione pelo menos uma opção',0));
+            }
+            if(parent::formularioValido($paginas_privadas))
+            {
+                array_push($this->form_valido, 
+                    parent::valida_int($dados['id'],'id','*id inválido',0),
+                    parent::valida_int($dados['cargo_id'],'cargo_id','*selecione uma opção',0),
+                    parent::valida_int($dados['nivel_id'],'nivel_id','*selecione uma opção',0),
+                    parent::valida_tamanho($dados['nome'],'nome','*O limite máximo permitido é 70 caracteres',70,0),
+                    parent::valida_bool($dados['ativo'],'ativo','*selecione uma opção')
+                );
+                
+                if (parent::formularioValido($this->form_valido)) {
 
-                    $msg = "Funcionário atualizado com sucesso!";
-                    $_SESSION['msg'] = parent::alertaSucesso($msg);
+                    $this->conn->beginTransaction();
+                    try {
+                        unset($dados['btn_atualizar']);
+                        $dados['dt_registro'] = date('Y-m-d H:i:s');
+                        $this->funcionario_paginas = $dados['pg_privada_id'];
+                        unset($dados['pg_privada_id']);
 
-                } catch (PDOException $e) {
+                        parent::implementar(
+                        "UPDATE funcionario SET
+                        id =:id, nome =:nome, cargo_id = :cargo_id, nivel_id = :nivel_id, ativo = :ativo, dt_registro = :dt_registro
+                        WHERE id =:id", $dados);
 
-                    $msg = "Não foi possivel atualizar o funcionário, tente novamente!";
-                    $_SESSION['msg'] = parent::alertaSucesso($msg);
+                        $id_funcionario_pagina['funcionario_id'] = $dados['id'];
+
+                        parent::implementar(
+                        "DELETE FROM funcionario_pg_privada 
+                        WHERE (funcionario_id = :funcionario_id)",
+                        $id_funcionario_pagina);
+                
+                        foreach ($this->funcionario_paginas as $pagina) {
+
+                            $id_funcionario_pagina['pg_privada_id'] = $pagina;
+                            $id_funcionario_pagina['dt_registro'] = date('Y-m-d H:i:s');
+
+                            parent::implementar(
+                            "INSERT INTO funcionario_pg_privada (funcionario_id, pg_privada_id, dt_registro)
+                            VALUES (:funcionario_id, :pg_privada_id, :dt_registro)",$id_funcionario_pagina);
+                        
+                        }
+                     
+                        $msg = "Funcionário atualizado com sucesso!";
+                        $_SESSION['msg'] = parent::alertaSucesso($msg);
+                        $this->conn->commit();
+
+                    } catch (PDOException $e) {
+
+                        $this->conn->rollBack();
+                        $msg = "Não foi possivel atualizar o funcionário, tente novamente!";
+                        $_SESSION['msg'] = parent::alertaSucesso($msg);
+                        header("Location:" . URL . "funcionario/editar/&id=" . $dados['id']);
+                        exit();
+                    }
+                    
+                } else {
+                    $msg = "Preencha corretamente todos os campos e tente novamente!";
+                    $_SESSION['msg'] = parent::alertaFalha($msg);
                     header("Location:" . URL . "funcionario/editar/&id=" . $dados['id']);
                     exit();
                 }
-            } else {
-                $msg = "Preencha corretamente todos os campos e tente novamente!";
+            }
+            else{
+                $msg = "Não foi possivel atualizar, verifique as páginas de acesso!";
                 $_SESSION['msg'] = parent::alertaFalha($msg);
                 header("Location:" . URL . "funcionario/editar/&id=" . $dados['id']);
                 exit();
             }
         } else {
+            
             $msg = "Não foi possivel atualizar, verifique todos os campos e tente novamente!";
             $_SESSION['msg'] = parent::alertaFalha($msg);
             header("Location:" . URL . "funcionario/editar/&id=" . $dados['id']);
@@ -201,14 +271,21 @@ class Funcionario extends Model
         }
     }
 
-    public function listarDetalhesAjax()
+    public function listarDetalhesAjax($funcionario_id)
     {
         try {
+            $id['id'] = $funcionario_id;
             $dados['cargo'] = parent::projetarTodos("SELECT id, nome FROM cargo");
             $dados['nivel'] = parent::projetarTodos("SELECT id, nome FROM nivel");
+            $dados['f_paginas'] = parent::projetarExpecifico(
+            "SELECT pg_privada_id AS f_paginas_id
+            FROM funcionario_pg_privada
+            WHERE funcionario_id = :id",$id,false);
+            $dados['paginas'] = parent::projetarTodos("SELECT id, nome FROM pg_privada");
+
             return $dados;
         } catch (PDOException $e) {
-            $msg = "Não foi possivel carregar os dados dos funcionários, caso o problema persistir entre em contato com o desenvolvedor";
+            $msg = "Não foi possivel carregar os dados do funcionário, caso o problema persistir entre em contato com o desenvolvedor";
             $_SESSION['msg'] = parent::alertaFalha($msg);
             exit();
         }
