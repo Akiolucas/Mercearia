2b6ebeab350a5178c5000cf8a3700f1255ae233c
Lucas Akio <lucasakio.td@gmail.com>
2021-09-11 16:46:52 -0300

    máscara para preço e kilograma

--- app/Assets/js/mask.js
+++ app/Assets/js/mask.js
@@ -1,2 +1,16 @@
 let maskCnpj = $('#form-cnpj');
 maskCnpj.mask('00.000.000/0000-00');
+
+let maskPreco = $('#form-preco');
+if(maskPreco.val() == "")
+{
+    maskPreco.val('0.10');
+}
+maskPreco.mask('000.000.000.000,00',{reverse:true});
+
+let maskKilograma = $('#form-kilograma');
+if(maskKilograma.val() == "")
+{
+    maskKilograma.val('0.001');
+}
+maskKilograma.mask('000.000.000.000,000',{reverse:true});
