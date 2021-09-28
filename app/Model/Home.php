c8ab68b736157d61834055c4d2409d5157abba84
Lucas Akio <lucasakio.td@gmail.com>
2021-09-27 21:41:12 -0300

    Model: Home, detalhes do sistema

--- app/Model/Home.php
+++ app/Model/Home.php
@@ -17,12 +17,63 @@ class Home extends Model
     {
         try{
             $home = parent::projetarTodos(
-               "SELECT
-               (SELECT count(funcionario.id) FROM funcionario) AS funcionario,
-               (SELECT count(fornecedor.id) FROM fornecedor) AS fornecedor,
-               (SELECT sum(estoque.quantidade) FROM estoque) AS produto,
-               (SELECT sum(caixa_fechamento.total) FROM caixa_fechamento) AS caixa"
-            );
+    "SELECT
+	(SELECT count(funcionario.id) 
+    FROM funcionario) AS funcionario,
+    
+    (SELECT count(funcionario.id) 
+    FROM funcionario
+    WHERE ativo = 1) AS funcionario_ativo,
+    
+	(SELECT count(fornecedor.id)
+    FROM fornecedor) AS fornecedor,
+    
+    (SELECT f.nome
+	FROM fornecedor f
+	INNER JOIN produto p
+	ON p.fornecedor_id = f.id
+	INNER JOIN estoque e
+	ON e.produto_id = p.id
+	GROUP BY p.fornecedor_id           
+	ORDER BY SUM(e.quantidade) DESC  
+	LIMIT 1) AS maiorFornecedor,
+    
+	(SELECT SUM(e.quantidade) + SUM(c.quantidade)
+	FROM fornecedor f
+	INNER JOIN produto p
+	ON p.fornecedor_id = f.id
+	INNER JOIN estoque e
+	ON e.produto_id = p.id
+	INNER JOIN caixa c
+	ON c.produto_id = p.id
+	GROUP BY p.fornecedor_id           
+	ORDER BY SUM(e.quantidade) DESC  
+	LIMIT 1) AS fornecedor_qtd,
+    
+	(SELECT sum(estoque.quantidade) 
+    FROM estoque) AS produto,
+    
+	(SELECT p.nome
+	FROM caixa c
+	INNER JOIN produto p
+	ON c.produto_id = p.id
+	GROUP BY c.produto_id           
+	ORDER BY SUM(c.quantidade) DESC   
+	LIMIT 1 ) AS maisVendido,
+    
+	(SELECT SUM(c.quantidade)
+	FROM caixa c
+	GROUP BY c.produto_id           
+	ORDER BY SUM(c.quantidade) DESC   
+	LIMIT 1 ) AS totalVendido,
+    
+    (SELECT sum(total) 
+    FROM caixa_fechamento) AS caixa,
+    
+    (SELECT sum(total)
+    FROM caixa_fechamento
+    WHERE dt_registro  LIKE CONCAT(curdate(),'%')) AS caixa_hoje"
+    );
     
             if (!empty($home)) {
                 return $home;
