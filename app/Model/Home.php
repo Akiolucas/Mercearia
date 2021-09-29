050f60f486eda47c8d8ee7ee80047cad85e06e25
Lucas Akio <lucasakio.td@gmail.com>
2021-09-29 20:51:31 -0300

    Model: Home, projeção dos dados atualizada

--- app/Model/Home.php
+++ app/Model/Home.php
@@ -18,14 +18,28 @@ class Home extends Model
         try{
             $home = parent::projetarTodos(
     "SELECT
-	(SELECT count(funcionario.id) 
+	(SELECT COUNT(funcionario.id) 
     FROM funcionario) AS funcionario,
     
-    (SELECT count(funcionario.id) 
+    (SELECT COUNT(funcionario.id) 
     FROM funcionario
     WHERE ativo = 1) AS funcionario_ativo,
     
-	(SELECT count(fornecedor.id)
+    (SELECT c.nome
+	FROM  funcionario f
+	INNER JOIN cargo c
+	ON f.cargo_id = c.id
+	GROUP BY (f.cargo_id)
+	ORDER BY COUNT(f.cargo_id) DESC LIMIT 1) AS cargo,
+    
+    (SELECT COUNT(f.cargo_id)
+	FROM  funcionario f
+	INNER JOIN cargo c
+	ON f.cargo_id = c.id
+	GROUP BY (f.cargo_id)
+	ORDER BY COUNT(f.cargo_id) DESC LIMIT 1) AS cargo_qtd,
+    
+	(SELECT COUNT(fornecedor.id)
     FROM fornecedor) AS fornecedor,
     
     (SELECT f.nome
@@ -50,7 +64,7 @@ class Home extends Model
 	ORDER BY SUM(e.quantidade) DESC  
 	LIMIT 1) AS fornecedor_qtd,
     
-	(SELECT sum(estoque.quantidade) 
+	(SELECT SUM(estoque.quantidade) 
     FROM estoque) AS produto,
     
 	(SELECT p.nome
@@ -66,13 +80,28 @@ class Home extends Model
 	GROUP BY c.produto_id           
 	ORDER BY SUM(c.quantidade) DESC   
 	LIMIT 1 ) AS totalVendido,
+
+	(SELECT e.quantidade
+	FROM caixa c
+	INNER JOIN estoque e
+	ON c.produto_id = e.produto_id
+	GROUP BY c.produto_id           
+	ORDER BY SUM(c.quantidade) DESC   
+	LIMIT 1 ) AS estoque,
     
-    (SELECT sum(total) 
-    FROM caixa_fechamento) AS caixa,
-    
-    (SELECT sum(total)
+    (SELECT SUM(total)
     FROM caixa_fechamento
-    WHERE dt_registro  LIKE CONCAT(curdate(),'%')) AS caixa_hoje"
+    WHERE dt_registro  LIKE CONCAT(curdate(),'%')) AS caixa_hoje,
+    
+    (SELECT SUM(quantidade)
+    FROM caixa
+    WHERE dt_registro  LIKE CONCAT(curdate(),'%')) AS caixa_hoje_qtd,
+    
+	(SELECT SUM(quantidade) 
+    FROM caixa) AS caixa_vendas,
+    
+    (SELECT SUM(total) 
+    FROM caixa_fechamento) AS caixa"
     );
     
             if (!empty($home)) {
