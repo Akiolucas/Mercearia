d3dc954d5e0d3d9c012da47b7f8709babf6ca681
Lucas Akio <lucasakio.td@gmail.com>
2021-09-30 11:00:55 -0300

    Remoção dos valores predefinidos

--- app/Assets/js/mask.js
+++ app/Assets/js/mask.js
@@ -2,22 +2,10 @@ let maskCnpj = $('#form-cnpj');
 maskCnpj.mask('00.000.000/0000-00');
 
 let maskPreco = $('#form-preco');
-if(maskPreco.val() == "")
-{
-    maskPreco.val('0.10');
-}
 maskPreco.mask('000.000.000.000,00',{reverse:true});
 
 let maskKilograma = $('#form-kilograma');
-if(maskKilograma.val() == "")
-{
-    maskKilograma.val('0.001');
-}
 maskKilograma.mask('000.000.000.000,000',{reverse:true});
 
 let maskLitro = $('#form-litro');
-if(maskLitro.val() == "")
-{
-    maskLitro.val('0.250');
-}
 maskLitro.mask('000.000.000.000,000',{reverse:true});
